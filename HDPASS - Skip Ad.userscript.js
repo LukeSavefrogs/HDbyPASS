7b2a83a05bd85026484bc01b1ced749f0afc3b50
Luca Salvarani <33452387+LukeSavefrogs@users.noreply.github.com>
2020-11-08 17:00:25 +0100

    Update HDPASS - Skip Ad.userscript.js

--- HDPASS - Skip Ad.userscript.js
+++ HDPASS - Skip Ad.userscript.js
@@ -1,1 +1,29 @@
+// ==UserScript==
+// @name         Streaming - Skip Ads (hdpass.*)
+// @namespace    http://tampermonkey.net/
+// @version      1.0.0
+// @description  try to take over the world!
+// @author       Luke Savefrogs
+// @include      /https:\/\/hdpass.\w[a-zA-Z]+\w\/movie/
+// @grant        none
+// ==/UserScript==
 
+(function() {
+    'use strict';
+
+    if(confirm("Andare all'inizio del film?")) {
+        document.querySelector(".fake-signup-close.open-fake-url").click();
+
+        var fake_player = document.getElementById("fake-player-wrapper");
+        if(!!fake_player) {
+            window.setTimeout(() => {
+                while (fake_player.firstChild) {
+                    fake_player.removeChild(fake_player.lastChild);
+                }
+                fake_player.remove();
+
+                console.log("Rimosso il Fake Player")
+            }, 2000);
+        }
+    }
+})();
