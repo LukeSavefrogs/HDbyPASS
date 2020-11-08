5c13a6fbb86f1eb80d974df6af456484dcefa9f8
Luca Salvarani <33452387+LukeSavefrogs@users.noreply.github.com>
2020-11-08 17:36:27 +0100

    Added history for version 1.0.0

--- HDPASS - Skip Ad.user.js
+++ HDPASS - Skip Ad.user.js
@@ -3,6 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @downloadURL  https://github.com/LukeSavefrogs/HDbyPASS/raw/main/HDPASS%20-%20Skip%20Ad.user.js
 // @version      1.0.0
+// @history      1.0.0 - Uploaded on GitHub
 // @description  Remove that 15 seconds fake player in some streaming players
 // @author       Luke Savefrogs
 // @include      /https:\/\/hdpass.\w[a-zA-Z]+\w\/movie/
